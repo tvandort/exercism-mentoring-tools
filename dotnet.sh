9d5c517fd606928b66ac68f9d429a051a1b1e523
Tom <tom@vandort.dev>
2020-01-02 16:00:34 -0500

    Initial mentor file.

---
+++ dotnet.sh
@@ -0,0 +1,9 @@
+function mentor-dotnet() {
+	echo "$(pwd -P)"
+	TARGET_DIR=$(pwd -P)
+	TEST_FILE=$(find $TARGET_DIR -type f -name \*Test.cs)
+	echo "Removing skips: $TEST_FILE"
+	sed -i 's/Fact(Skip = "Remove to run test")/Fact/g' $TEST_FILE
+	docker run -it --rm -v "$(pwd -P)":/test microsoft/dotnet dotnet test /test
+}
+
